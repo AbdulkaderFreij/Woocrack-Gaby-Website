abebc2f0561578965e9fc6a9d216533bafa92577
Marwa <marwa@marwa.localdomain>
2020-05-18 12:04:47 +0300

    changed Donate button color

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+import PropTypes from "prop-types";
+import React, { Component } from "react";
 import {
   Button,
   Container,
@@ -10,33 +10,32 @@ import {
   Segment,
   Sidebar,
   Visibility,
-} from 'semantic-ui-react'
-import './Nav.css';
-import {Link} from "react-router-dom";
+} from "semantic-ui-react";
+import "./Nav.css";
+import { Link } from "react-router-dom";
 
 const getWidth = () => {
-  const isSSR = typeof window === 'undefined'
+  const isSSR = typeof window === "undefined";
 
-  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
-}
+  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth;
+};
 
 class DesktopContainer extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-            isLoggedIn:false,
-            activeItem: "home"
-		}
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      activeItem: "home",
+    };
+  }
 
-  hideFixedMenu = () => this.setState({ fixed: false })
-  showFixedMenu = () => this.setState({ fixed: true })
- 
+  hideFixedMenu = () => this.setState({ fixed: false });
+  showFixedMenu = () => this.setState({ fixed: true });
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name });
   render() {
-    const { children } = this.props
-    const { fixed } = this.state
+    const { children } = this.props;
+    const { fixed } = this.state;
     const { activeItem } = this.state;
     return (
       <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
@@ -47,64 +46,111 @@ class DesktopContainer extends Component {
         >
           <Segment
             inverted
-            textAlign='center'
-            style={{ height: 100, padding: '1em 0em' }}
+            textAlign="center"
+            style={{ height: 100, padding: "1em 0em" }}
             vertical
           >
             <Menu
-              fixed={fixed ? 'top' : null}
+              fixed={fixed ? "top" : null}
               inverted={!fixed}
               pointing={!fixed}
               secondary={!fixed}
-              size='large'
+              size="large"
             >
               <Container>
-                <Menu.Item as={Link} to='/' name="home" active={activeItem === "home"} onClick={this.handleItemClick}>
+                <Menu.Item
+                  as={Link}
+                  to="/"
+                  name="home"
+                  active={activeItem === "home"}
+                  onClick={this.handleItemClick}
+                >
                   Home
                 </Menu.Item>
-                <Menu.Item as={Link} to='/themes' name="themes" active={activeItem === "themes"} onClick={this.handleItemClick}>Themes</Menu.Item>
-                <Menu.Item as={Link} to='/plugins' name="plugins" active={activeItem === "plugins"} onClick={this.handleItemClick}>Plugins</Menu.Item>
-                <Menu.Item as={Link} to='/contactus' name="contactus" active={activeItem === "contactus"} onClick={this.handleItemClick}>Contact Us</Menu.Item>
-                {this.state.isLoggedIn ?  <Menu.Item position='right'>
-                        <Button as='a' inverted={!fixed}>Logout</Button>
-                        </Menu.Item>
-                  : <Menu.Item position='right'>
-                  <Button as='a' inverted={!fixed}> Login</Button>
-                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>Register</Button>
-                  </Menu.Item>}
+                <Menu.Item
+                  as={Link}
+                  to="/themes"
+                  name="themes"
+                  active={activeItem === "themes"}
+                  onClick={this.handleItemClick}
+                >
+                  Themes
+                </Menu.Item>
+                <Menu.Item
+                  as={Link}
+                  to="/plugins"
+                  name="plugins"
+                  active={activeItem === "plugins"}
+                  onClick={this.handleItemClick}
+                >
+                  Plugins
+                </Menu.Item>
+                <Menu.Item
+                  as={Link}
+                  to="/contactus"
+                  name="contactus"
+                  active={activeItem === "contactus"}
+                  onClick={this.handleItemClick}
+                >
+                  Contact Us
+                </Menu.Item>
+                {this.state.isLoggedIn ? (
+                  <Menu.Item position="right">
+                    <Button as="a" inverted={!fixed}>
+                      Logout
+                    </Button>
+                  </Menu.Item>
+                ) : (
+                  <Menu.Item position="right">
+                    <Button as="a" inverted={!fixed}>
+                      {" "}
+                      Login
+                    </Button>
+                    <Button
+                      as="a"
+                      inverted={!fixed}
+                      primary={fixed}
+                      style={{
+                        marginLeft: "0.5em",
+                        backgroundColor: "#2185d0",
+                      }}
+                    >
+                      Donate
+                    </Button>
+                  </Menu.Item>
+                )}
               </Container>
             </Menu>
-            
           </Segment>
         </Visibility>
 
         {children}
       </Responsive>
-    )
+    );
   }
 }
 
 DesktopContainer.propTypes = {
   children: PropTypes.node,
-}
+};
 
 class MobileContainer extends Component {
-    constructor(props) {
-		super(props);
-		this.state = {
-            isLoggedIn:false,
-            activeItem: "home"
-		}
-	}
-    handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      activeItem: "home",
+    };
+  }
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
-  handleSidebarHide = () => this.setState({ sidebarOpened: false })
+  handleSidebarHide = () => this.setState({ sidebarOpened: false });
 
-  handleToggle = () => this.setState({ sidebarOpened: true })
+  handleToggle = () => this.setState({ sidebarOpened: true });
 
   render() {
-    const { children } = this.props
-    const { sidebarOpened } = this.state
+    const { children } = this.props;
+    const { sidebarOpened } = this.state;
     const { activeItem } = this.state;
 
     return (
@@ -115,48 +161,82 @@ class MobileContainer extends Component {
       >
         <Sidebar
           as={Menu}
-          animation='push'
+          animation="push"
           inverted
           onHide={this.handleSidebarHide}
           vertical
           visible={sidebarOpened}
         >
-                <Menu.Item as={Link} to='/' name="home" active={activeItem === "home"} onClick={this.handleItemClick}>
-                  Home
-                </Menu.Item>
-                <Menu.Item as={Link} to='/themes' name="themes" active={activeItem === "themes"} onClick={this.handleItemClick}>Themes</Menu.Item>
-                <Menu.Item as={Link} to='/plugins' name="plugins" active={activeItem === "plugins"} onClick={this.handleItemClick}>Plugins</Menu.Item>
-                <Menu.Item as={Link} to='/contactus' name="contactus" active={activeItem === "contactus"} onClick={this.handleItemClick}>Contact Us</Menu.Item>
-                {this.state.isLoggedIn ?  <Menu.Item >
-                       Logout
-                        </Menu.Item>
-                  : <><Menu.Item >
-                   Login
-                   </Menu.Item>
-                   <Menu.Item>
-                  Register
-                  </Menu.Item></>}
+          <Menu.Item
+            as={Link}
+            to="/"
+            name="home"
+            active={activeItem === "home"}
+            onClick={this.handleItemClick}
+          >
+            Home
+          </Menu.Item>
+          <Menu.Item
+            as={Link}
+            to="/themes"
+            name="themes"
+            active={activeItem === "themes"}
+            onClick={this.handleItemClick}
+          >
+            Themes
+          </Menu.Item>
+          <Menu.Item
+            as={Link}
+            to="/plugins"
+            name="plugins"
+            active={activeItem === "plugins"}
+            onClick={this.handleItemClick}
+          >
+            Plugins
+          </Menu.Item>
+          <Menu.Item
+            as={Link}
+            to="/contactus"
+            name="contactus"
+            active={activeItem === "contactus"}
+            onClick={this.handleItemClick}
+          >
+            Contact Us
+          </Menu.Item>
+          {this.state.isLoggedIn ? (
+            <Menu.Item>Logout</Menu.Item>
+          ) : (
+            <>
+              <Menu.Item>Login</Menu.Item>
+              <Menu.Item>Register</Menu.Item>
+            </>
+          )}
         </Sidebar>
 
         <Sidebar.Pusher dimmed={sidebarOpened}>
           <Segment
             inverted
-            textAlign='center'
-            style={{ minHeight: 250, padding: '1em 0em' }}
+            textAlign="center"
+            style={{ minHeight: 250, padding: "1em 0em" }}
             vertical
           >
             <Container>
-              <Menu inverted pointing secondary size='large'>
+              <Menu inverted pointing secondary size="large">
                 <Menu.Item onClick={this.handleToggle}>
-                  <Icon name='sidebar' />
+                  <Icon name="sidebar" />
                 </Menu.Item>
-                {this.state.isLoggedIn ?  <Menu.Item position='right'>
-                        <Button as='a'>Logout</Button>
-                        </Menu.Item>
-                  : <Menu.Item position='right'>
-                  <Button as='a'> Login</Button>
-                  <Button as='a' style={{ marginLeft: '0.5em' }}>Register</Button>
-                  </Menu.Item>}
+                {this.state.isLoggedIn ? (
+                  <Menu.Item position="right">
+                    <Button as="a">Logout</Button>
+                  </Menu.Item>
+                ) : (
+                  <Menu.Item position="right">
+                    <Button as="a"> Login</Button>
+                    <Button as="a" style={{ marginLeft: "0.5em" }}>
+                      Register
+                    </Button>
+                  </Menu.Item>
+                )}
               </Menu>
             </Container>
           </Segment>
@@ -164,23 +244,23 @@ class MobileContainer extends Component {
           {children}
         </Sidebar.Pusher>
       </Responsive>
-    )
+    );
   }
 }
 
 MobileContainer.propTypes = {
   children: PropTypes.node,
-}
+};
 
 const Navbar = ({ children }) => (
   <div>
     <DesktopContainer>{children}</DesktopContainer>
     <MobileContainer>{children}</MobileContainer>
   </div>
-)
+);
 
 Navbar.propTypes = {
   children: PropTypes.node,
-}
+};
 
 export default Navbar;
